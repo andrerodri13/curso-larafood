7e532237f8f5637d348d197af53b38ea5bde741c
Andre <andrerodri13@hotmail.com>
2021-05-16 13:29:55 -0300

    Modulo 14 Completo

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ route::prefix('admin')
     ->group(function () {
 
         /**
+         * Routes Tables
+         */
+//        Route::get('tables/qrcode/{identify}', 'TableController@qrcode')->name('tables.qrcode');
+        Route::any('tables/search', 'TableController@search')->name('tables.search');
+        Route::resource('tables', 'TableController');
+
+        /**
          * Product x Category
          */
         Route::get('products/{id}/category/{idCategory}/detach', 'CategoryProductController@detachCategoryProduct')->name('products.category.detach');
