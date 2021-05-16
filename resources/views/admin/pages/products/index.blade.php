052a6c2be1edbb8141a67d0ada4f865be2cc34c9
Andre <andrerodri13@hotmail.com>
2021-05-16 12:37:53 -0300

    Modulo 13 Completo

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -37,7 +37,7 @@
                             </td>
                             <td>{{ $product->title }}</td>
                             <td style="width:10px;">
-{{--                                <a href="{{ route('products.categories', $product->id) }}" class="btn btn-warning" title="Categorias"><i class="fas fa-layer-group"></i></a>--}}
+                                <a href="{{ route('products.categories', $product->id) }}" class="btn btn-primary"><i class="fas fa-layer-group"></i></a>
                                 <a href="{{ route('products.edit', $product->id) }}"  class="btn btn-info"><i class="fas fa-pen-alt"></i></a>
                                 <a href="{{ route('products.show', $product->id) }}" class="btn btn-warning"><i class="fas fa-eye"></i></a>
                             </td>
